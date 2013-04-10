8ed26b6541e8030c810953ac87f55f0f64e254a0
Morteza Shahriari Nia <info@MShahriariNia.com>
2013-04-10 13:57:06 -0400

    Simple Project 3 works.

--- spark3/src/main/scala/SimpleJob.scala
+++ spark3/src/main/scala/SimpleJob.scala
@@ -6,7 +6,7 @@ object SimpleJob extends Application {
   val logFile = "/var/log/syslog" // Should be some file on your system
     val cd : SparkContext = null
   val sc = new SparkContext("local", "Simple Job", "$YOUR_SPARK_HOME",
-    List("target/scala-2.9.2/simple-project_2.9.2-1.0.jar"))
+    List("target/scala-2.9.2/simple-project3_2.9.2-1.0.jar"))
   val logData = sc.textFile(logFile, 2).cache()
   val numAs = logData.filter(line => line.contains("a")).count()
   val numBs = logData.filter(line => line.contains("b")).count()
